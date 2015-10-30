571152aec56a127797ac6931b5dc49737a314855
Emmanuel Farrar <emmanuel.farrar@fullsail.edu>
2015-10-30 12:00:06 +0600

    Commit for Test 01

---
+++ Test_01/Resources/app.js
@@ -0,0 +1,16 @@
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Titanium.UI.setBackgroundColor('#000');
+
+
+//
+// create base UI tab and root window
+//
+var win1 = Titanium.UI.createWindow({  
+    title:'Tab 1',
+    backgroundColor:'#fff'
+});
+
+
+win1.open();
+
+
